b9cd5e46ca9ddd16a62e9de8674dc64cc5d87b99
trisnens <svenboerrigterbusiness@gmail.com>
2025-03-27 19:48:20 +0100

    init

---
+++ src/custom_bang.js
@@ -0,0 +1,20 @@
+window.custom_bangs = [
+    {
+        c: "General",
+        d: "duckduckgo.com",
+        r: 0,
+        s: "Search the bang shortcuts",
+        sc: "Bang",
+        t: "bang",
+        u: "https://duckduckgo.com/bangs?q={{{s}}}",
+    },
+    {
+        c: "AI",
+        d: "chatgpt.com",
+        r: 0,
+        s: "Ask ChatGPT",
+        sc: "ChatGPT",
+        t: "gpt",
+        u: "https://chatgpt.com/?q={{{s}}}",
+    },
+]
